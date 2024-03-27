25be507a7ce45e38fef4b7eab501375bfee4b5b1
Ivy <ivy7661@gmail.com>
2024-03-27 23:14:56 +0800

    Updates

---
+++ assets/star_empty-B9KiXxT8.js
@@ -0,0 +1,1 @@
+const s=""+new URL("../icons/star_full.svg",import.meta.url).href,t=""+new URL("../icons/star_empty.svg",import.meta.url).href;export{s as _,t as a};
--- assets/star_full--3WgZ6nB.js
+++
@@ -1,1 +0,0 @@
-const r=""+new URL("../icons/star_full.svg",import.meta.url).href;export{r as _};
