2bad46d2320b00a05692cb9e3b9f2c9609f07483
Ivy <ivy7661@gmail.com>
2024-04-01 17:04:14 +0800

    search card

--- src/views/front/SearchMovie.vue
+++ src/views/front/SearchMovie.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 搜尋框 -->
-  <div class="row justify-content-center my-3">
+  <div class="row justify-content-center mx-2 mx-lg-0 my-3">
     <div class="col-md-6 d-flex flex-row align-items-center">
       <img src="/icons/search.svg" alt="search" height="25" class="" />
       <input
@@ -18,32 +18,34 @@
   </div>
   <!-- 搜尋結果 -->
   <div v-for="movie in filterMovies" :key="movie.id">
-    <div class="d-flex justify-content-center align-items-center my-5">
-      <router-link :to="`movies/${movie.id}`">
-        <div class="movie-card d-flex flex-column flex-lg-row text-white px-4 py-5">
-          <div class="d-flex justify-content-center mb-3 mb-lg-0 me-lg-5">
-            <img :src="movie.image" alt="電影海報" class="poster" />
-          </div>
-          <div>
-            <div class="d-flex flex-column align-items-center align-items-lg-start">
-              <h3>{{ movie.name }}</h3>
-              <h4>{{ movie.englishName }}</h4>
-              <div class="wrap-star mb-3">
-                <span v-for="i in movie.ratingStars" :key="i + 123" class="mx-1">
-                  <img src="/icons/star_full.svg" alt="star-full" width="30" height="30" />
-                </span>
-                <span v-for="i in 5 - movie.ratingStars" :key="i + 123" class="mx-1">
-                  <img src="/icons/star_empty.svg" alt="star-empty" width="30" height="30" />
-                </span>
-              </div>
+    <div class="row justify-content-center align-items-center my-5">
+      <div class="col-11 col-md-8">
+        <router-link :to="`movies/${movie.id}`">
+          <div class="movie-card d-flex flex-column flex-lg-row text-white px-4 py-5">
+            <div class="d-flex justify-content-center mb-3 mb-lg-0 me-lg-5">
+              <img :src="movie.image" alt="電影海報" class="poster" />
             </div>
-            <p>語言：{{ movie.language }}</p>
-            <p>片長：{{ movie.movieLength }}</p>
-            <p>導演：{{ movie.staff.director }}</p>
-            <p>主演：{{ movie.staff.actor }}</p>
+            <div>
+              <div class="d-flex flex-column align-items-center align-items-lg-start">
+                <h3>{{ movie.name }}</h3>
+                <h4>{{ movie.englishName }}</h4>
+                <div class="wrap-star mb-3">
+                  <span v-for="i in movie.ratingStars" :key="i + 123" class="mx-1">
+                    <img src="/icons/star_full.svg" alt="star-full" width="30" height="30" />
+                  </span>
+                  <span v-for="i in 5 - movie.ratingStars" :key="i + 123" class="mx-1">
+                    <img src="/icons/star_empty.svg" alt="star-empty" width="30" height="30" />
+                  </span>
+                </div>
+              </div>
+              <p>語言：{{ movie.language }}</p>
+              <p>片長：{{ movie.movieLength }}</p>
+              <p>導演：{{ movie.staff.director }}</p>
+              <p>主演：{{ movie.staff.actor }}</p>
+            </div>
           </div>
-        </div>
-      </router-link>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -66,9 +68,9 @@ export default {
     ...mapState(movieStore, ['movies']),
     filterMovies() {
       if (!this.keyWord && !this.selected) {
-        return []; // 如果沒有輸入關鍵字且未選擇dropdown，則返回空陣列
+        return [];
       }
-      const strArr = this.keyWord.split(' '); // 以空白格切分字串
+      const strArr = this.keyWord.split(' ');
       const arr = []; // 篩選出搜尋結果的陣列
       // 比對字串
       strArr.forEach((str) => {
@@ -80,7 +82,6 @@ export default {
           }
         });
       });
-      // 如果輸入兩個關鍵字就會出現重複的資料，所以需要刪除重複資料。
       // 過濾出重複的元素
       return [...new Set(arr)];
     }
@@ -93,7 +94,6 @@ export default {
 
 <style lang="scss" scoped>
 .movie-card {
-  width: 760px;
   height: 380px;
   border-radius: 20px;
   border: 2px solid #9498fe;
